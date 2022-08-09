9c225304b9a323fafe1a7f43c80e83a7b3ce1c54
Daniel Grayvold <85690571+dgrayvold@users.noreply.github.com>
2022-08-09 16:17:24 -0500

    About section sketching

--- src/App.vue
+++ src/App.vue
@@ -11,56 +11,24 @@
 	/>
 
 	<main>
-		<section ref="initialSection" id="about">
-			<WavesIcon class="w-24 h-24 mx-auto mb-8 text-theme-800" />
-			<h2 class="text-theme-800">What I am</h2>
+		<section id="about" ref="initialSection" class="grid grid-cols-2">
+			<div
+				class="w-100 h-100 mx-8 rounded-xl inline-block bg-black"
+			></div>
 
-			<ul class="grid grid-cols-3 gap-16 space-around">
-				<li>
-					<h3 class="text-2xl text-center mb-4 text-theme-800">
-						Designer
-					</h3>
-					<p>
-						The web is full of generic, messy, slow, and
-						inaccessible sites, but it doesn't have to be. I create
-						designs that are bold and unique but don't compromise on
-						performance or a visitor's ability to experience them. I
-						combine color, typography, structure, and media to
-						provide experiences that delight and provide value to
-						people.
-					</p>
-				</li>
-
-				<li>
-					<h3 class="text-2xl text-center mb-4 text-theme-800">
-						Developer
-					</h3>
-					<p>
-						Great designs come to life when they are made
-						interactive and engaging. I use whatever tools and
-						languages necessary to add function to form.
-					</p>
-					<p>
-						I also know the value of a well-crafted automation. My
-						scripts have saved my colleagues and myself hundreds of
-						hours of tedious labor by utilizing the capabilities of
-						Goo&shy;gle Apps Script, VBA, Apple&shy;Script/JXA,
-						Bash, and more.
-					</p>
-				</li>
-
-				<li>
-					<h3 class="text-2xl text-center mb-4 text-theme-800">
-						Creator
-					</h3>
-					<p>
-						My greatest strength is my varied professional
-						background. I'm not just a designer and developer. I'm
-						also a musician, a photographer, a video editor, a print
-						designer, a writer, and much more.
-					</p>
-				</li>
-			</ul>
+			<div class="max-w-128">
+				<WavesIcon class="w-8 h-8 mb-0 -ml-0.5 text-theme-800" />
+				<p class="text-2xl">Lorem ipsum dolor sit amet.</p>
+				<p class="text-2xl">
+					Interdum mattis fringilla eleifend semper imperdiet fames
+					mattis purus.
+				</p>
+				<p class="text-2xl">
+					Dictum id tempor odio interdum est aliquam luctus a at.
+					Interdum tempus sapien quam platea molestie nibh non etiam
+					turpis justo.
+				</p>
+			</div>
 		</section>
 
 		<section id="projects" class="bg-theme-800">
