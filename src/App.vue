a6f71f4a6a4539c71f3fc5819ccadca37af41122
Daniel Grayvold <85690571+dgrayvold@users.noreply.github.com>
2022-08-28 20:47:50 -0500

    Separate projects list into component

--- src/App.vue
+++ src/App.vue
@@ -29,107 +29,11 @@
 			<SailboatIcon class="w-10 h-10 !stroke-theme-700 stroke-3 col-span-2" />
 			<h2 class="text-theme-100 col-span-2 mb-0">What I've made</h2>
 
-			<ul
-				class="
-					col-span-2
-					xl:col-span-1
-					max-h-108
-					mb-8
-					rounded-xl
-					bg-theme-700 bg-opacity-30
-					border-1 border-theme-100 border-opacity-50
-					overflow-y-scroll
-				"
-			>
-				<li
-					v-for="project in projects"
-					:key="project.project"
-					class="text-theme-100 transition-colors"
-					even=" bg-theme-600 bg-opacity-10"
-					hover="bg-theme-800 bg-opacity-100"
-					:class="{
-						'!bg-opacity-100 !bg-theme-900': activeProject.project == project.project,
-					}"
-				>
-					<button
-						class="w-full text-left cursor-pointer"
-						p="x-2 y-1"
-						@click="activeProject = projects.find(p => p.project == project.project)"
-					>
-						<h3 class="text-2xl">
-							{{ project.project }}
-						</h3>
-						<p class="text-sm leading-2">{{ project.location }}</p>
-
-						<ul class="flex gap-2 my-2">
-							<li
-								v-for="tag in project.tags"
-								:key="tag"
-								class="px-1.5 text-theme-900 bg-theme-600 rounded uppercase text-xs"
-							>
-								{{ tag }}
-							</li>
-						</ul>
-					</button>
-				</li>
-			</ul>
-
-			<figure
-				class="
-					relative
-					col-span-2
-					xl:col-span-1
-					mb-8
-					rounded-xl
-					bg-theme-900 bg-opacity-50
-					border-1 border-theme-100 border-opacity-50
-					text-theme-100
-					overflow-hidden
-				"
-			>
-				<img
-					:src="activeProject.loadedImage?.src ?? activeProject.image"
-					class="
-						border-b-1 border-theme-100 border-opacity-50
-						w-full
-						h-72
-						object-cover object-top
-					"
-				/>
-
-				<p class="text-2xl px-2">{{ activeProject.project }}</p>
-				<p class="px-2">{{ activeProject.description }}</p>
-
-				<a
-					v-show="activeProject.url != null"
-					:href="activeProject.url"
-					target="_blank"
-					class="
-						inline-block
-						bg-theme-700
-						mx-2
-						px-2
-						pb-1
-						my-2
-						rounded
-						text-theme-900
-						group
-					"
-				>
-					<span class="leading-4 align-middle pr-1">{{ activeProject.cta ?? 'Go' }}</span>
-					<SailboatIcon
-						class="
-							inline-block
-							align-middle
-							h-4
-							stroke-2
-							transition-all
-							transform
-							group-hover:translate-x-2
-						"
-					/>
-				</a>
-			</figure>
+			<ProjectsList
+				:projects="projects"
+				:activeProject="activeProject"
+				@select="setActiveProject"
+			/>
 		</section>
 
 		<section id="contact" class="relative text-center pb-32 overflow-hidden">
@@ -159,6 +63,7 @@
 
 <script>
 import Header from '@/components/Header.vue';
+import ProjectsList from '@/components/ProjectsList.vue';
 import Snowflake from '@/components/Snowflake.vue';
 import ContactForm from '@/components/ContactForm.vue';
 import AnchorIcon from '@/components/Icons/AnchorIcon.vue';
@@ -174,6 +79,7 @@ import projects from '/src/projects.json';
 export default {
 	components: {
 		Header,
+		ProjectsList,
 		ContactForm,
 		LighthouseIcon,
 		WavesIcon,
@@ -214,6 +120,10 @@ export default {
 			const offset = element.getBoundingClientRect().top;
 
 			ui.smoothScroll(offset, 90);
+		},
+
+		setActiveProject(name) {
+			this.activeProject = this.projects.find(project => project.project == name);
 		},
 
 		diveIn() {
---
+++ src/components/ProjectsList.vue
@@ -0,0 +1,151 @@
+<template>
+	<ul
+		class="
+			col-span-2
+			xl:col-span-1
+			max-h-108
+			mb-8
+			rounded-xl
+			bg-theme-700 bg-opacity-30
+			border-1 border-theme-100 border-opacity-50
+			overflow-y-scroll
+		"
+	>
+		<li
+			v-for="project in projects"
+			:key="project.project"
+			class="text-theme-100 transition-colors"
+			even=" bg-theme-600 bg-opacity-10"
+			hover="bg-theme-800 bg-opacity-100"
+			:class="{
+				'!bg-opacity-100 !bg-theme-900': activeProject.project == project.project,
+			}"
+		>
+			<button
+				class="w-full text-left cursor-pointer"
+				p="x-2 y-1"
+				@click="selectProject(project)"
+			>
+				<h3 class="text-2xl">
+					{{ project.project }}
+				</h3>
+				<p class="text-sm leading-2">{{ project.location }}</p>
+
+				<ul class="flex gap-2 my-2">
+					<li
+						v-for="tag in project.tags"
+						:key="tag"
+						class="px-1.5 text-theme-900 bg-theme-600 rounded uppercase text-xs"
+					>
+						{{ tag }}
+					</li>
+				</ul>
+			</button>
+		</li>
+	</ul>
+
+	<figure
+		class="
+			relative
+			col-span-2
+			xl:col-span-1
+			mb-8
+			rounded-xl
+			bg-theme-900 bg-opacity-50
+			border-1 border-theme-100 border-opacity-50
+			text-theme-100
+			overflow-hidden
+		"
+	>
+		<div v-if="activeProject.loadedImage || activeProject.image">
+			<img
+				:src="activeProject.loadedImage?.src ?? activeProject.image"
+				class="
+					border-b-1 border-theme-100 border-opacity-50
+					w-full
+					h-72
+					object-cover object-top
+				"
+			/>
+
+			<p class="text-2xl px-2">{{ activeProject.project }}</p>
+			<p class="px-2">{{ activeProject.description }}</p>
+
+			<a
+				v-show="activeProject.url != null"
+				:href="activeProject.url"
+				target="_blank"
+				class="inline-block bg-theme-700 mx-2 px-2 pb-1 my-2 rounded text-theme-900 group"
+			>
+				<span class="leading-4 align-middle pr-1">{{ activeProject.cta ?? 'Go' }}</span>
+				<SailboatIcon
+					class="
+						inline-block
+						align-middle
+						h-4
+						stroke-4
+						transition-all
+						transform
+						group-hover:translate-x-2
+					"
+				/>
+			</a>
+		</div>
+		<div v-else class="flex flex-col items-center bg-theme-900 bg-opacity-50 w-full h-full">
+			<SailboatIcon class="h-32 w-32 mx-auto my-8 stroke-theme-800" />
+
+			<p class="text-3xl text-center">{{ activeProject.project }}</p>
+			<p class="py-8 px-8 max-w-128">{{ activeProject.description }}</p>
+
+			<a
+				v-show="activeProject.url != null"
+				:href="activeProject.url"
+				target="_blank"
+				class="inline-block bg-theme-700 mx-2 px-2 pb-1 my-2 rounded text-theme-900 group"
+			>
+				<span class="leading-4 align-middle pr-1">{{ activeProject.cta ?? 'Go' }}</span>
+				<SailboatIcon
+					class="
+						inline-block
+						align-middle
+						h-4
+						stroke-4
+						transition-all
+						transform
+						group-hover:translate-x-2
+					"
+				/>
+			</a>
+		</div>
+	</figure>
+</template>
+
+<script>
+import SailboatIcon from '@/components/Icons/SailboatIcon.vue';
+
+export default {
+	components: {
+		SailboatIcon,
+	},
+
+	props: {
+		activeProject: {
+			type: Object,
+			default: () => ({}),
+		},
+		projects: {
+			type: Array,
+			default: () => [],
+		},
+	},
+
+	emits: ['select'],
+
+	methods: {
+		selectProject(project) {
+			// activeProject = projects.find(p => p.project == project.project);
+			this.$emit('select', project.project);
+		},
+	},
+};
+</script>
