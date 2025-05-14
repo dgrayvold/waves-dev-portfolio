26e09926dd63decdf4d1015b465d362339636055
Daniel Grayvold <85690571+dgrayvold@users.noreply.github.com>
2025-05-14 13:15:50 -0500

    Adjust contact email message

--- server/api/contact.ts
+++ server/api/contact.ts
@@ -25,7 +25,7 @@ export default defineEventHandler(async event => {
 	const runtimeConfig = useRuntimeConfig();
 	const resend = new Resend(runtimeConfig.resendKey);
 	const { name, email, message } = formValidationResult.data;
-	const text = `You have a new contact form submission from ${name} (${email}). The message is: \n\n${message}`;
+	const text = `You have a new contact form submission from ${name} at ${email}. The message is: \n\n${message}`;
 
 	const { error: emailError } = await resend.emails.send({
 		from: `NoReply <${runtimeConfig.fromEmail}>`,
